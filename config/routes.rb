b1505b626c9ea947c2f62ff56c450f01712cfd6f
Roberto Barros <roberto.f.barros@gmail.com>
2020-08-18 11:10:35 -0300

    livecode

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+  resources :restaurants
+
+
+  # # CREATE
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+
+  # # READ (ALL / ONE)
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # # UPDATE
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # # DELETE
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+end
